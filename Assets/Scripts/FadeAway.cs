1fd4d8bda2c2b7e86be520ff40c7f186b035d66f
Scott Bromander <scott@primeacademy.io>
2016-06-21 15:47:34 -0500

    initial commit

---
+++ Assets/Scripts/FadeAway.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeAway : MonoBehaviour {
+	private CountdownTimer countdownTimer;
+	private Text textUI;
+	public int fadeDuration = 5;
+	private bool fading = false;
+
+	// Use this for initialization
+	void Start () {
+		textUI = GetComponent<Text> ();
+		countdownTimer = GetComponent<CountdownTimer> ();
+
+		StartFading (fadeDuration);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (fading) {
+			float alphaRemaining = countdownTimer.GetProportionTimeRemaining ();
+			print (alphaRemaining);
+			Color c = textUI.material.color;
+			c.a = alphaRemaining;
+			textUI.material.color = c;
+
+			if (alphaRemaining < 0.01) {
+				//If under the threshold value, go ahead and make completely invisable. 
+				c.a = 0;
+				textUI.material.color = c;
+				print (c);
+				fading = false;
+			}
+		}
+	}
+
+	public void StartFading(int timerTotal) {
+		countdownTimer.ResetTimer (timerTotal);
+		fading = true;
+	}
+}
